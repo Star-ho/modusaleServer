1a7461b6fc04d4496b5c78de014a28ddcfde49d5
Star-ho <58648988+Star-ho@users.noreply.github.com>
2021-09-28 19:44:57 +0900

    쿠팡 이미지 URL 추가

--- src/main/java/com/modusaleJava/server/utils/ImgSourceToHTML.java
+++ src/main/java/com/modusaleJava/server/utils/ImgSourceToHTML.java
@@ -7,20 +7,27 @@ public class ImgSourceToHTML {
     public String imgToHTML(String imgStr){
         ModusaleMapper modusaleMapper=new ModusaleMapper();
         String html="<html><head></head><body>\n";
-        Map<String, String> imgMap=modusaleMapper.jsonToObj(imgStr, LinkedHashMap.class);
-        List<String> keyList=new ArrayList<>(imgMap.keySet());
-        Collections.sort(keyList);
-        for(int i=0;i<keyList.size();i++) {
-            html+="<div style='float: left;margin: 10;width: 45%;' ><img style='float: left;background-color: blueviolet;' src="
-                    + imgMap.get(keyList.get(i)) + " width='250' height='100' />" +
-                    "<p>" + keyList.get(i) + "</p>" +
-                    "</div>\n";
-            if(i%5==0){
+        HashMap<String, String> imgMap=modusaleMapper.jsonToObj(imgStr, HashMap.class);
+        List<String> keyList = new ArrayList<>(imgMap.keySet());
+        keyList.sort((s1, s2)->s1.compareTo(s2));
+        String imgURL="<div style=\"float: left;margin: 10;height: 30;width: 45%;\"></div>";
+
+        for(String key:keyList) {
+            if(key=="line"){
                 html+="<div style='float: left;margin: 10;width: 100%;' >'";
                 html+="<p>------------------------------------------------------------------------------------------------------------</p>";
                 html+="</div>";
+                continue;
+            }
+            html+="<div style='float: left;margin: 10;width: 45%;' ><img style='float: left;background-color: blueviolet;' src="
+                    + imgMap.get(key) + " width='250' height='100' />" +
+                    "<p>" + key + "</p>" +
+                    "</div>\n";
+            if(key.contains("eats://")){
+                imgURL+="<p>/"+imgMap.get(key).split("//")[2]+"</p>";
             }
         }
+        html+=imgURL;
         html+="</body></html>";
         return html;
     }
