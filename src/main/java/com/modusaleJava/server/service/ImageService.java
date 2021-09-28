edfb9fcda68c7549e58a38cb14d7c47a76ed8fab
Star-ho <58648988+Star-ho@users.noreply.github.com>
2021-09-28 19:45:16 +0900

    카프카 리스너 설정 변경

--- src/main/java/com/modusaleJava/server/service/ImageService.java
+++ src/main/java/com/modusaleJava/server/service/ImageService.java
@@ -1,20 +1,16 @@
 package com.modusaleJava.server.service;
 
 import com.modusaleJava.server.utils.ImgSourceToHTML;
-import com.modusaleJava.server.utils.ModusaleMapper;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
 
 @Service
 public class ImageService {
     private String imgStr="";
 
-    @KafkaListener(topics ="imgData", groupId = "web-server")
+    @KafkaListener(topics ="imgData", containerFactory = "kafkaListenerContainerFactory")
     public void consume(String message) throws IOException {
         ImgSourceToHTML imgSourceToHTML=new ImgSourceToHTML();
         this.imgStr=imgSourceToHTML.imgToHTML(message);
