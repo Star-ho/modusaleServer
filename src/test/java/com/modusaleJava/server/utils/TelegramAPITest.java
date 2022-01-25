9846fa63f83d9e74e9509b3b61b49d1bb999d226
Star-ho <58648988+Star-ho@users.noreply.github.com>
2022-01-25 22:24:24 +0900

    test : 텔레그램 전송 테스트

---
+++ src/test/java/com/modusaleJava/server/utils/TelegramAPITest.java
@@ -0,0 +1,24 @@
+package com.modusaleJava.server.utils;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@SpringBootTest(properties = {"job.autorun.enabled=false"})
+@ExtendWith(SpringExtension.class)
+class TelegramAPITest{
+
+    private TelegramAPI telegramAPI;
+
+    public TelegramAPITest(@Autowired TelegramAPI telegramAPI){
+        this.telegramAPI=telegramAPI;
+    }
+
+    @Test
+    public void sendTestMessage() throws InterruptedException {
+        telegramAPI.send("test!");
+        Thread.sleep(3000);
+    }
+}
