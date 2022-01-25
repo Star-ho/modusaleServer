9f69ac08fdbd63bd4f207402236cf3e1b7d0b41a
Star-ho <58648988+Star-ho@users.noreply.github.com>
2022-01-25 22:53:40 +0900

    fix : 커맨드러너 클래스로 따로 뺌

--- src/main/java/com/modusaleJava/server/ServerApplication.java
+++ src/main/java/com/modusaleJava/server/ServerApplication.java
@@ -1,10 +1,7 @@
 package com.modusaleJava.server;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 /**
@@ -18,22 +15,5 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 public class ServerApplication {
     public static void main(String[] args) {
         SpringApplication.run(ServerApplication.class, args);
-    }
-
-    private ModusaleBatch modusaleBatch;
-
-    @Autowired
-    public void setModusaleBatch(ModusaleBatch modusaleBatch) {
-        this.modusaleBatch = modusaleBatch;
-    }
-
-    @Bean
-    public CommandLineRunner dataLoader(){
-        return new CommandLineRunner() {
-            @Override
-            public void run(String... args) {
-                modusaleBatch.consume();
-            }
-        };
     }
 }
