09d01c75490663938bf682eb295066f684cbb380
Star-ho <58648988+Star-ho@users.noreply.github.com>
2022-02-06 22:33:52 +0900

    refactor : 시작시 실행안하도록 설정 추가

--- src/main/java/com/modusale/ModusaleDataLoader.java
+++ src/main/java/com/modusale/ModusaleDataLoader.java
@@ -2,9 +2,11 @@ package com.modusale;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.stereotype.Component;
 
 @Component
+@ConditionalOnProperty(prefix = "job.autorun", name = "enabled", havingValue = "true", matchIfMissing = true)
 public class ModusaleDataLoader implements CommandLineRunner {
     private final ModusaleBatch modusaleBatch;
 
